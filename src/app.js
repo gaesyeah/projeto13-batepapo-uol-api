0736e5677de540e64d3842212ee780542119bd05
Gabriel Goes Santos Rios <gabrielgoesrios@gmail.com>
2023-06-28 12:40:25 -0300

    POST e GET /participants implementado

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import cors from 'cors';
+import dayjs from 'dayjs';
 import dotenv from 'dotenv';
 import express from 'express';
+import joi from 'joi';
 import { MongoClient } from 'mongodb';
 
 //configuração do servidor
@@ -16,10 +18,58 @@ const {DATABASE_URL, PORT} = process.env;
 const mongoClient = new MongoClient(DATABASE_URL);
 let db;
 mongoClient.connect()
-    .then(() => db = mongoClient.db)
+    .then(() => {
+        db = mongoClient.db();
+        console.log(`Conectado ao banco ${DATABASE_URL}`)
+    })
     .catch(({message}) => console.log(message))
 ;
 
+//POST participants
+app.post('/participants', (req, res) => {
+    const {name} = req.body;
+    
+    const schema = joi.string().required();
+    const {error} = schema.validate(name);
+    if (error){
+        return res.status(422).send(error.message);
+    }
 
+    db.collection('participants').findOne({name})
+        .then(resp => {
+            if (resp){
+                return res.sendStatus(409)
+            }
+
+            db.collection('participants').insertOne({name, lastStatus: Date.now()})
+                .then(() => {
+                    db.collection('messages').insertOne(
+                        {from: name, to: 'Todos', text: 'entra na sala...', type: 'status', time: dayjs(Date.now()).format('HH:mm:ss')}
+                    )
+                        .then(() => res.sendStatus(201))
+                        .catch(({message}) => res.status(500).send(message))
+                    ;  
+                })
+                .catch(({message}) => res.status(500).send(message))
+        })
+        .catch(({message}) => res.status(500).send(message))
+    ;
+});
+
+//GET participants
+app.get('/participants', (req, res) => {
+    db.collection('participants').find().toArray()
+        .then(data => res.send(data))
+        .catch(({message}) => res.status(500).send(message))
+    ;  
+});
+
+/* //POST messages
+app.get('/messages', (req, res) => {
+    const {to, text, type} = req.body;
+    const {User} = req.headers;
+
+    //fazer verificações com a biblioteca joi
+}); */
 
 app.listen(PORT, () => console.log(`Rodando em http://localhost:${PORT}`));
