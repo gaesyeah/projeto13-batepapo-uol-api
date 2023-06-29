6b560e3a047b2f04604a01947eacee7f028c4d08
Gabriel Goes Santos Rios <gabrielgoesrios@gmail.com>
2023-06-28 22:59:07 -0300

    implementação do POST /messages

--- src/app.js
+++ src/app.js
@@ -16,14 +16,13 @@ const { DATABASE_URL, PORT } = process.env;
 
 //configuração do mongodb(bando de dados)
 const mongoClient = new MongoClient(DATABASE_URL);
-let db;
-mongoClient.connect()
-  .then(() => {
-    db = mongoClient.db();
-    console.log(`Conectado ao banco ${DATABASE_URL}`)
-  })
-  .catch(({ message }) => console.log(message))
-;
+try {
+  await mongoClient.connect();
+  console.log(`Conectado ao banco ${DATABASE_URL}`);
+} catch ({message}) {
+  console.log(message);
+}
+const db = mongoClient.db();
 
 //POST participants
 app.post('/participants', async (req, res) => {
@@ -57,10 +56,29 @@ app.get('/participants', (req, res) => {
 });
 
 //POST messages
-app.get('/messages', (req, res) => {
+app.get('/messages', async (req, res) => {
   const { to, text, type } = req.body;
   const { user } = req.headers;
 
+  const messageSchema = joi.object({
+    to: joi.string().required(),
+    text: joi.string().required(),
+    type: joi.valid('message','private_message')
+  });
+  const {error} = messageSchema.validate({to, text, type});
+
+  try {
+    const participant = await db.collection('participants').findOne({ name: user });
+    if (!participant || error) return res.sendStatus(422);
+
+    db.collection('messages').insertOne(
+      {from: user, to, text, type, time: dayjs(Date.now()).format('HH:mm:ss')}
+    );
+    res.sendStatus(201);
+
+  } catch ({message}){
+    res.status(500).send(message);
+  }
 });
 
 app.listen(PORT, () => console.log(`Rodando em http://localhost:${PORT}`));
